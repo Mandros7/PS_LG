2ae66da5a2d608d02c097327380ce953c5a6607f
Hector Rodriguez <hectorrogue.9@gmail.com>
2018-10-02 21:15:12 +0200

    JS Structure

---
+++ Ejercicio2/js/script.js
@@ -0,0 +1,25 @@
+function initAnimation () {
+    //This function sets a loading animation
+}
+
+function showData () {
+    //This function unsets the loading animation
+    //and populates graphs
+}
+
+function parseData() {
+    //This function normalizes the datasets
+}
+
+function retrieveData() {
+    //This function retrieves and parses data
+    // AJAX
+    // return parseData()
+}
+
+async function initialize() {
+    //This funcion can be called periodically (if protected against timeouts)
+    //initAnimation
+    //wait retrieveData
+    //showData
+}
