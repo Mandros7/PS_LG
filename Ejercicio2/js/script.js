73c7fd8330e42eb92ecb24ef1b836fe753b1ea53
Hector Rodriguez <hectorrogue.9@gmail.com>
2018-10-03 21:03:29 +0200

    Added ajax requests

--- Ejercicio2/js/script.js
+++ Ejercicio2/js/script.js
@@ -1,25 +1,54 @@
+
+var loadJSON = function(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
+      xhr.responseType = 'json';
+      xhr.onload = function() {
+        var status = xhr.status;
+        if (status == 200) {
+          resolve(xhr.response);
+        } else {
+          reject(xhr.response);
+        }
+      };
+      xhr.onerror = function() {
+        reject(xhr.response);
+      };
+      xhr.send();
+    });
+  }
+
 function initAnimation () {
     //This function sets a loading animation
+    var frame = document.getElementById("loading");
+    frame.style.opacity = 1;
 }
 
-function showData () {
+function showData (data) {
     //This function unsets the loading animation
     //and populates graphs
+    var frame = document.getElementById("loading");
+    frame.style.opacity = 0;
+    console.log(data);
 }
 
-function parseData() {
+function parseData(json) {
     //This function normalizes the datasets
+    return json;
 }
 
 function retrieveData() {
-    //This function retrieves and parses data
-    // AJAX
-    // return parseData()
+    Promise.all([
+        loadJSON("http://s3.amazonaws.com/logtrust-static/test/test/data1.json"),
+        loadJSON("http://s3.amazonaws.com/logtrust-static/test/test/data2.json"),
+        loadJSON("http://s3.amazonaws.com/logtrust-static/test/test/data3.json")
+    ]).then(function(jsonData) {
+        showData(parseData(jsonData));
+    }); 
 }
 
-async function initialize() {
-    //This funcion can be called periodically (if protected against timeouts)
-    //initAnimation
-    //wait retrieveData
-    //showData
+function initialize() {
+    initAnimation();
+    retrieveData();
 }
