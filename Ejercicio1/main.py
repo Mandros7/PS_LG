4579d9b444a1170d052fc1603405f7679e46acd4
Hector Rodriguez <hectorrogue.9@gmail.com>
2018-10-05 12:17:19 +0200

    Added main function

---
+++ Ejercicio1/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import classifynumbers
+
+##################################
+# Main fucntion to tests         #
+##################################
+
+if __name__ == '__main__':
+    numberList = [0,123,"2",2.56,10,6,28384,234]
+    classifynumbers.classify_numbers(numberList)
